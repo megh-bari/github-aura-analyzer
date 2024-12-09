1c028cdc2d696a0229cccb3528f584b76da917cd
Megh Bari <meghbari01@gmail.com>
2024-12-09 11:19:59 +0530

    Modified

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,20 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
