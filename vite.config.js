228d9631ecc0ad3237121b573bc5b046654afb37
Megh Bari <meghbari01@gmail.com>
2024-12-09 11:39:05 +0530

    deploying

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,16 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
+    outDir: 'dist', // Ensure output directory is 'dist' for Vercel deployment
+  },
 })
